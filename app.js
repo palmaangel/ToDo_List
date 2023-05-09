01d17ae9bf8b5fe451d18efd5a778b7327b8e071
Angel Palma <palmajangel@gmail.com>
2023-05-08 22:50:56 -0600

    added some simple css for the list part, that way its not distractingly ugly

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var toDoItem = prompt("What do you want to add?");
-var buttons = document.querySelectorAll('#todoContainer #todo-button');
+//var toDoItem = prompt("What do you want to add?");
+var buttons = document.querySelectorAll('.todoItemList #todo-button');
 const addItem = document.querySelector('#todoContainer .todoItemList');
 
 buttons.forEach(function(button){
@@ -11,4 +11,7 @@ buttons.forEach(function(button){
 
 });
 
-addItem.innerHTML += '<li> <span class="item">' , toDoItem , '</span> <span id="todo-button">REMOVE</span></li>'
+//addItem.innerHTML += '<li> <span class="item">' , toDoItem , '</span> <span id="todo-button">REMOVE</span></li>'
+const Where = document.querySelector(parent.Element'#todo-button')
+
+console.log(Where)
